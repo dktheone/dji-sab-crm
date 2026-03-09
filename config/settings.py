cfb670cde6d60cb5654a593a6db14ed8e1d9e429
dktheone <dktheonecoder@gmail.com>
2026-03-09 16:22:37 +0530

    Update ALLOWED_HOSTS in settings.py

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ DEBUG = str2bool(os.environ.get('DEBUG'))
 ALLOWED_HOSTS = ['*']
 
 # Add here your deployment HOSTS
-CSRF_TRUSTED_ORIGINS = ['http://crm.sabhospitality.com', 'http://18.222.186.28', 'http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
+CSRF_TRUSTED_ORIGINS = ['http://crm.sabhospitality.com', 'http://3.18.101.156', 'http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
 
 #Render Context
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
